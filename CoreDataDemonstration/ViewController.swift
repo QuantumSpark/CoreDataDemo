a8a1cf18cd090c63d0be5709c0b2ae2f733f28ef
James Park <QuantumSpark@jamess-macbook-pro.local>
2016-09-28 19:35:40 -0700

    Initial Commit

---
+++ CoreDataDemonstration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemonstration
+//
+//  Created by James Park on 2016-09-28.
+//  Copyright © 2016 James Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
